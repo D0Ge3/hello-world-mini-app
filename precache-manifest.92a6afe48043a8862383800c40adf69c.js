66d78823d3df105a647c1acacbdd0caeeb626f5c
d0ge3 <fnik340@gmail.com>
2020-06-20 22:22:43 +0300

    Updates

---
+++ precache-manifest.92a6afe48043a8862383800c40adf69c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3409e4bf28f9b7da6fa27a6395772374",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c54eb8e62c1eab8f6223",
+    "url": "/static/css/2.6bab1db4.chunk.css"
+  },
+  {
+    "revision": "d1a97d80775866a90b35",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "c54eb8e62c1eab8f6223",
+    "url": "/static/js/2.c2d86d3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c2d86d3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1a97d80775866a90b35",
+    "url": "/static/js/main.c183a874.chunk.js"
+  },
+  {
+    "revision": "1550bc5cb6224d84350f",
+    "url": "/static/js/runtime-main.74ac9606.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
